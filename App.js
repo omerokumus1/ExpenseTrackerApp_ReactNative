55837a3d1c09fabc930afc313b6e044631d81c29
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-09 15:36:57 +0300

    main navigation skeleton is constructed

--- App.js
+++ App.js
@@ -1,5 +1,43 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
 import { Text } from 'react-native';
+import RecentExpensesScreen from './screens/RecentExpensesScreen';
+import AllExpensesScreen from './screens/AllExpensesScreen';
+import ManageExpenseScreen from './screens/ManageExpenseScreen';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen
+        name="RecentExpensesScreen"
+        component={RecentExpensesScreen}
+      />
+      <BottomTabs.Screen
+        name="AllExpensesScreen"
+        component={AllExpensesScreen}
+      />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen
+            name="ManageExpenseScreen"
+            component={ManageExpenseScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
