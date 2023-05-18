c56221096ad836cc70211e39c2d7b20e8c4afec6
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-18 11:00:46 +0300

    LoadingOverlay for ManageExpenseScreen

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,12 +1,14 @@
-import { useContext, useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { GlobalStyles } from '../constants/styles';
 import IconButton from '../components/UI/IconButton';
 import { ExpensesContext } from '../store/expenses-context';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 import { deleteExpenese, storeExpense, updateExpense } from '../util/http';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
 
 function ManageExpenseScreen({ navigation, route }) {
+  const [isStateLoading, setIsStateLoading] = useState(false);
   const expensesCtx = useContext(ExpensesContext);
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
@@ -20,8 +22,10 @@ function ManageExpenseScreen({ navigation, route }) {
   }, [navigation, isEditing]);
 
   async function deleteExpenseHandler() {
+    setIsStateLoading(true);
     expensesCtx.deleteExpense(editedExpenseId);
     await deleteExpenese(editedExpenseId);
+    //setIsStateLoading(false);
     navigation.goBack();
   }
 
@@ -30,14 +34,20 @@ function ManageExpenseScreen({ navigation, route }) {
   }
 
   async function confirmHandler(expenseData) {
+    setIsStateLoading(true);
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
       await updateExpense(editedExpenseId, expenseData);
+      //setIsStateLoading(false);
     } else {
       const id = await storeExpense(expenseData);
       expensesCtx.addExpense({ ...expenseData, id });
     }
     navigation.goBack();
+  }
+
+  if (isStateLoading) {
+    return <LoadingOverlay />;
   }
 
   return (
